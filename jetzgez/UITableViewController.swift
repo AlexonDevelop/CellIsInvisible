4c41d9fa532f8414068d08e266064715b05be6cf
AlexonDevelop <business@alexpichler.com>
2016-09-05 01:42:19 +0200

    Add files via upload

---
+++ jetzgez/UITableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jetzgez
+//
+//  Created by Alex Pichler on 05.09.16.
+//  Copyright © 2016 Alex Pichler. All rights reserved.
+//
+
+import UIKit
+
+class UITableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //some Code
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
